c320bc299d4a059fc1d9c8bf5d66e4d40d8a03b3
oustriX <50222966+oustriX@users.noreply.github.com>
2022-04-17 18:30:34 +1000

    test refactoring

--- tconv_test.go
+++ tconv_test.go
@@ -7,22 +7,33 @@ import (
 
 func TestCalculte(t *testing.T) {
 	req := require.New(t)
+	t.Run("Zero to Fahrenheit", func(t *testing.T) {
+		tconv := calculate(0, "-f")
+		req.Equal(float64(32), tconv)
+	})
 
-	tconv := calculate(0, "-f")
-	req.Equal(float64(32), tconv)
+	t.Run("Positive to Fahrenheit", func(t *testing.T) {
+		tconv := calculate(10, "-f")
+		req.Equal(float64(50), tconv)
+	})
 
-	tconv = calculate(10, "-f")
-	req.Equal(float64(50), tconv)
+	t.Run("Negative to Fahrenheit", func(t *testing.T) {
+		tconv := calculate(-10, "-f")
+		req.Equal(float64(14), tconv)
+	})
 
-	tconv = calculate(-10, "-f")
-	req.Equal(float64(14), tconv)
+	t.Run("Zero to Celsius", func(t *testing.T) {
+		tconv := calculate(0, "-c")
+		req.Equal(float64(-17.78), tconv)
+	})
 
-	tconv = calculate(0, "-c")
-	req.Equal(float64(-17.78), tconv)
+	t.Run("Positive to Celsius", func(t *testing.T) {
+		tconv := calculate(10, "-c")
+		req.Equal(float64(-12.22), tconv)
+	})
 
-	tconv = calculate(10, "-c")
-	req.Equal(float64(-12.22), tconv)
-
-	tconv = calculate(-10, "-c")
-	req.Equal(float64(-23.33), tconv)
+	t.Run("Negative to Celsius", func(t *testing.T) {
+		tconv := calculate(-10, "-c")
+		req.Equal(float64(-23.33), tconv)
+	})
 }
